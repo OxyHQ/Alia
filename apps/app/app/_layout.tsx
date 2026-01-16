9a5c42a81e123f5b1c093abe321e5104d1366239
Nate Isern Alvarez <nate.isern.alvarez@gmail.com>
2026-01-16 14:09:46 +0100

    fix(layout): update initial route name to '(app)' for better navigation

--- apps/app/app/_layout.tsx
+++ apps/app/app/_layout.tsx
@@ -20,7 +20,7 @@ export {
 } from 'expo-router';
 
 export const unstable_settings = {
-  initialRouteName: 'index',
+  initialRouteName: '(app)',
 };
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
